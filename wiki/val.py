151c5c74cb19177f605fac9f7d943e35d6062010
dbcerigo <dbcerigo@gmail.com>
2017-08-25 21:39:15 +0100

    Add NaN safe SMAPE version
    NOTE: the original version is ~1/3 faster than this one

--- wiki/val.py
+++ wiki/val.py
@@ -5,3 +5,12 @@ def smape(y_true, y_pred):
     diff = np.abs(y_true - y_pred) / denominator
     diff[denominator == 0] = 0.0
     return np.mean(diff)
+
+def smape_df(df, y_true_label, y_pred_label):
+    df = df.dropna(subset=[y_true_label])
+    y_true = df[y_true_label]
+    y_pred = df[y_pred_label]
+    denominator = (y_true + y_pred) / 200.0
+    diff = np.abs(y_true - y_pred) / denominator
+    diff[denominator == 0] = 0.0
+    return np.mean(diff)
