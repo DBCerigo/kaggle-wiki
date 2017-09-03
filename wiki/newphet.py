0acda46737974383255b14ca1b82eb4b59d4e8e4
dbcerigo <dbcerigo@gmail.com>
2017-09-03 22:42:32 +0100

    Update plotting to show better original vs predicted data
    * now relies on y_org and yhat as main plots
    * includes new optional args for plotting further data

--- wiki/newphet.py
+++ wiki/newphet.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 
-def prophet_plot(df, ax=None, uncertainty=True, plot_cap=True, 
+def prophet_plot(df, ax=None, uncertainty=True, plot_cap=True, plot_yhat_org=False, plot_y=True,
          xlabel='ds',
-         ylabel='y'):
+         ylabel='y_org'):
     """Plot the Prophet forecast.
     Parameters
     ----------
@@ -24,15 +24,19 @@ def prophet_plot(df, ax=None, uncertainty=True, plot_cap=True,
         fig = ax.get_figure()
     df_train = df[df['train'] == 1]
     df_val = df[df['train'] == 0]
-    ax.plot(df_train['ds'].values, df_train['y'], 'k')
-    ax.plot(df_val['ds'].values, df_val['y'], 'r')
+    ax.plot(df_train['ds'].values, df_train['y_org'], 'k')
+    ax.plot(df_val['ds'].values, df_val['y_org'], 'r')
     ax.plot(df['ds'].values, df['yhat'], ls='-', c='#0072B2')
+    if plot_y:
+        ax.plot(df_train['ds'].values, df_train['y'], c='#3CB371', alpha=0.2)
+    if plot_yhat_org:
+        ax.plot(df_train['ds'].values, df_train['yhat_org'], c='#00FFFF', alpha=0.2)
     if 'cap' in df and plot_cap:
         ax.plot(df['ds'].values, df['cap'], ls='--', c='k')
     if uncertainty:
         ax.fill_between(df['ds'].values, df['yhat_lower'],
                         df['yhat_upper'], color='#0072B2',
-                        alpha=0.2)
+                        alpha=0.15)
     ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
