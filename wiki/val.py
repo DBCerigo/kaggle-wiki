5b013870a28d43a2e8f4163120c1118545993289
At Dbc <atdbcdata@gmail.com>
2017-09-04 17:26:18 +0000

    Fix smape abs bug

--- wiki/val.py
+++ wiki/val.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def smape(y_true, y_pred):
     # NOTE: should check and make sure that NaNs aren't included
-    denominator = (y_true + y_pred) / 200.0
+    denominator = (np.abs(y_true) + np.abs(y_pred)) / 200.0
     diff = np.abs(y_true - y_pred) / denominator
     diff[denominator == 0] = 0.0
     return np.mean(diff)
@@ -11,7 +11,7 @@ def smape_df(df, y_true_label, y_pred_label):
     df = df.dropna(subset=[y_true_label])
     y_true = df[y_true_label]
     y_pred = df[y_pred_label]
-    denominator = (y_true + y_pred) / 200.0
+    denominator = (np.abs(y_true) + np.abs(y_pred)) / 200.0
     diff = np.abs(y_true - y_pred) / denominator
     diff[denominator == 0] = 0.0
     return np.mean(diff)
