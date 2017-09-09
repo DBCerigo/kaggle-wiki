b5239de3d1a8048e9e2b067834fc0572b9702f9e
dbcerigo <dbcerigo@gmail.com>
2017-09-09 10:45:08 -0500

    Add func for shifting period to previous year

--- wiki/utils.py
+++ wiki/utils.py
@@ -1,5 +1,15 @@
 import time
 
+def prevYear_shift(period):
+    year = 365
+    assert period[0]
+    new1 = period[0] - year
+    if not period[1]:
+        new2 = -year
+    else:
+        new2 = period[1] - year
+    return (new1,new2)
+
 class clock(object):
     def __enter__(self):
         self.start = time.time()
